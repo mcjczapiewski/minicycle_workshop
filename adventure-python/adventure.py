058bd0be2f8019501e20bf578479a5c7e1d83da8
Robert Wadelek <robert.wadelek.in@gmail.com>
2020-04-18 15:20:41 +0200

    adding help

--- adventure-python/adventure.py
+++ adventure-python/adventure.py
@@ -1,4 +1,4 @@
-
+import random
 
 player = {
     'room': 'outside',
@@ -11,14 +11,14 @@ rooms = {
         "description": "You are standing outside of a huge cave entrance.",
         "east": "castle",
         "west": "cave",
-        "move posibility": "There are following exits: east, west",
+        "move possibility": "east, west",
         "items": ["key"]
     },
     "cave": {
         "title": "Cave",
         "description": "You're in a cave.",
         "east": "outside",
-        "move posibility": "There are following exits: east",
+        "move possibility": "east",
         "items": ["rocks", "torch", "rope", "coins"]
     },
     "castle":{
@@ -27,25 +27,26 @@ rooms = {
         "east": "hall",
         "west": "outside",
         "north": "front room",
-        "move posibility": "There are following exits: east, west, north",
+        "move possibility": "east, west, north",
         "items": ["sword"]
     },
     "front room":{
         "title": "Front room",
         "description": "You're getting to the front room in the Castle",
         "south": "castle",
-        "move posibility": "There are following exits: south",
+        "move possibility": "south",
         "items": ["chest", "coins"]
     },
     "hall":{
         "title": "Hall",
         "description": "You're in the Hall in the Castle",
         "west": "castle",
-        "move posibility": "There are following exits: west",
+        "move possibility": "west",
         "items": ["key"]
     }
 
 }
+
 
 def movement_validation(command):
     if command == 'w':
@@ -64,7 +65,9 @@ def movement(command): #pokazuje możliwości ruchu
     next_position = rooms[current_position][command]
     player['room'] = next_position
 
+
 def main():
+    print("Type 'help' for a list of commands.")
     describe_room()
     playing = True
     while playing:
@@ -80,10 +83,24 @@ def main():
             describe_room()
         elif command in ['get', 'g'] or 'get' in command:
             get_items(command)
-        elif command in ['info', 'i']:
+        elif command in ['inventory', 'i']:
             show_inventory()
+        elif command in ['help', 'h', '?']:
+            show_help()
         else:
             print(f'Unrecognized command: {command}')
+
+
+def show_help():
+    list_of_commands = [
+        'look, l',
+        rooms[player['room']]['move possibility'],
+        'get, g, get <item name>',
+        'inventory, i',
+        'quit, q'
+    ]
+    for command_option in list_of_commands:
+        print(command_option)
 
 
 def get_items(command):
@@ -91,10 +108,15 @@ def get_items(command):
         item_to_get = command.split(' ', 1)[1]
     else:
         item_to_get = input('Get what? ').lower()
-    player["inventory"].append(item_to_get)
-    print("Taken.")
-    # remove item from room
-    rooms[player['room']]['items'].remove(item_to_get)
+    
+    random_choice = random.randint(0,1)
+    if random_choice:       
+        player["inventory"].append(item_to_get)
+        print("Taken.")
+        # remove item from room
+        rooms[player['room']]['items'].remove(item_to_get)
+    else:
+        print("You didn't find it. Search again.")
 
 
 def show_inventory():
@@ -116,7 +138,7 @@ def describe_room():
     print(room['title'])
     print()
     print(room['description'])
-    print(room['move posibility'])
+    print(f"There are following exits: {room['move possibility']}")
     if room['items']:
         print(f'There are following items: {", ".join(room["items"])}')
 
