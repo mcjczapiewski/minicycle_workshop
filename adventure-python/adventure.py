18ddc887792d02a6fc8bc8ecad2dd16962811954
misiekWawa <michalorlowski@op.pl>
2020-04-18 14:10:31 +0200

    first hour

--- adventure-python/adventure.py
+++ adventure-python/adventure.py
@@ -8,13 +8,56 @@ rooms = {
     "outside": {
         "title": "Outside",
         "description": "You are standing outside of a huge cave entrance.",
+        "east": "castle",
+        "west": "cave",
+        "move posibility": "There are following exits: east, west"
     },
     "cave": {
         "title": "Cave",
         "description": "You're in a cave.",
+        "east": "outside",
+        "move posibility": "There are following exits: east"
     },
+    "castle":{
+        "title": "Castle",
+        "description": "You're getting to the castle",
+        "east": "hall",
+        "west": "outside",
+        "north": "front room",
+        "move posibility": "There are following exits: east, west, north"
+        
+    },
+    "front room":{
+        "title": "Front room",
+        "description": "You're getting to the front room in the Castle",
+        "south": "castle",
+        "move posibility": "There are following exits: south"
+    },
+    "hall":{
+        "title": "Hall",
+        "description": "You're in the Hall in the Castle",
+        "west": "castle",
+        "move posibility": "There are following exits: west"
+    }
+
 }
 
+def movement_validation(command):
+    if command == 'w':
+        command = 'west'
+    elif command == 'e':
+        command = 'east'
+    elif command == 's':
+        command = 'south'
+    elif command == 'n':
+        command = 'north'
+    return command
+
+
+def movement(command): #pokazuje możliwości ruchu
+    current_position = player['room']
+    next_position = rooms[current_position][command]
+    player['room'] = next_position
 
 def main():
     describe_room()
@@ -26,13 +69,17 @@ def main():
         elif command in ['quit', 'q']:
             print('Bye!')
             playing = False
+        elif command in ['west', 'w', 'east', 'e', 'south', 's', 'north', 'n']:
+            command = movement_validation(command)
+            movement(command)
+            describe_room()
         else:
             print(f'Unrecognized command: {command}')
 
 
 def get_command():
     print()
-    return input('> ')
+    return input('> ').lower()
 
 
 def describe_room():
@@ -41,6 +88,7 @@ def describe_room():
     print(room['title'])
     print()
     print(room['description'])
+    print(room['move posibility'])
 
 
 if __name__ == '__main__':
